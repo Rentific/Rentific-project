470b193d9d2bbc4a79cdf9ba53e27028c7e8822b
nimsirovic1 <37272011+nimsirovic1@users.noreply.github.com>
2021-03-31 22:26:59 +0200

    Revert "PR: Admin service (#2)"
    This reverts commit f04312612c7ffdd024daaf0c94a6a5bbaaeb6ca0.

--- adminservice/adminservice/src/main/java/com/example/adminservice/adminservice/Admin/ErrorHandling/InvalidRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.example.adminservice.adminservice.Admin.ErrorHandling;
-
-public class InvalidRequestException extends Exception {
-    public InvalidRequestException(String error) {
-        super(error);
-    }
-}
--- adminservice/adminservice/src/main/java/com/example/adminservice/adminservice/Admin/ErrorHandling/RealEstateNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.adminservice.adminservice.Admin.ErrorHandling;
-
-public class RealEstateNotFoundException extends Exception {
-    public RealEstateNotFoundException(String error) {
-        super(error);
-    }
-}
