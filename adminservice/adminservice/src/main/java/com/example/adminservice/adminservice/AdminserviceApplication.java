3cac1793ff338bcea20bbb0e4ec3f1900e7edbf7
nimsirovic1 <37272011+nimsirovic1@users.noreply.github.com>
2021-03-31 22:43:23 +0200

    Added Admin service

--- adminservice/adminservice/src/main/java/com/example/adminservice/adminservice/AdminserviceApplication.java
+++ adminservice/adminservice/src/main/java/com/example/adminservice/adminservice/AdminserviceApplication.java
@@ -3,9 +3,7 @@ package com.example.adminservice.adminservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @SpringBootApplication
 @EnableConfigurationProperties
 public class AdminserviceApplication {
