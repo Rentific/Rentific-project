26b66aa42124db2539c37bf08aa74878964a5145
sejlap <sejlapljakic233@gmail.com>
2021-04-15 16:52:33 +0200

    System-events service

---
+++ userservice/src/main/java/com/example/userservice/Interceptor/UserInterceptorAppConfig.java
@@ -0,0 +1,17 @@
+package com.example.userservice.Interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Component
+public class UserInterceptorAppConfig extends WebMvcConfigurerAdapter {
+    @Autowired
+    UserInterceptor userInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(userInterceptor);
+    }
+}
