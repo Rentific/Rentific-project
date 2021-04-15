af9ac1f4d7a26750004d93e500f1eb46086554e9
sejlap <sejlapljakic233@gmail.com>
2021-04-15 16:52:33 +0200

    System-events service

---
+++ system-events/src/main/java/com/example/systemevents/Repository/EventRepository.java
@@ -0,0 +1,10 @@
+package com.example.systemevents.Repository;
+import com.example.systemevents.Model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Integer> {
+
+}
