43a6637ac58e12eb2478701f6ecb42e107e1b2d2
nimsirovic1 <37272011+nimsirovic1@users.noreply.github.com>
2021-05-04 23:50:09 +0200

    Work in progress

---
+++ authentication-service/src/main/java/com/example/authenticationservice/Model/User.java
@@ -0,0 +1,40 @@
+package com.example.authenticationservice.Model;
+
+import javax.persistence.Column;
+
+public class User {
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+    private String status;
+
+    public User() {
+
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
