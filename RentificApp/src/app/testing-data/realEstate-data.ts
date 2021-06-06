b61ceb3232e0bc349e5fa3fef11744bc4411abd6
NermaHanic <37115420+NermaHanic@users.noreply.github.com>
2021-06-06 17:34:12 +0200

    User/nerma.hanic/frontend (#45)
    * Added UI unit tests for search service
    
    * Added UI test for rest of the services

---
+++ RentificApp/src/app/testing-data/realEstate-data.ts
@@ -0,0 +1,29 @@
+import { RealEstate } from "../_models/real-estate";
+
+export const dummyRealEstate1  : RealEstate = {
+    realEstateId : 1,
+    name : "Available real estate",
+    price : 120000,
+    address : "Test address",
+    country : "Test country",
+    city: "Test city",
+    description : "This is some test description",
+    isReservated: false,
+    staffId : 1,
+    state: 0,
+    imageModel: null
+}
+
+export const dummyRealEstate2  : RealEstate = {
+    realEstateId : 2,
+    name : "Not available test real estate",
+    price : 120000,
+    address : "Test address",
+    country : "Test country",
+    city: "Test city",
+    description : "This is some test description",
+    isReservated: true,
+    staffId : 1,
+    state: 0,
+    imageModel: null
+}
