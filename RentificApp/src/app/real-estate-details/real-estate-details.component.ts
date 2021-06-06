ccedc61ec98746367b094afafa21901ce8fa359a
nimsirovic1 <37272011+nimsirovic1@users.noreply.github.com>
2021-06-06 23:53:26 +0200

    UI fixes

--- RentificApp/src/app/real-estate-details/real-estate-details.component.ts
+++ RentificApp/src/app/real-estate-details/real-estate-details.component.ts
@@ -9,6 +9,7 @@ import { ImageModel } from '../_models/ImageModel';
 import { RealEstate } from '../_models/real-estate';
 import { Role } from '../_models/role';
 import { RealEstateService } from '../_services/real-estate.service';
+import { SearchService } from '../_services/search.service';
 
 @Component({
   selector: 'app-real-estate-details',
@@ -17,18 +18,32 @@ import { RealEstateService } from '../_services/real-estate.service';
 })
 export class RealEstateDetailsComponent implements OnInit {
   realEstate: RealEstate;
+  images: ImageModel[] = [];
   closeResult = '';
   imagesList: String[];
+  retrievedImage: any;
+  base64Data: any;
+  retrieveResonse: any;
   currentUserRole: Observable<Role>;
   currentUserEmail: string = localStorage.getItem("email");
   isAdmin: Boolean = false;
 
   constructor(private route: ActivatedRoute, private modalService: NgbModal, 
-    private router: Router, private http: HttpClient, private realEstateService: RealEstateService) {
+    private router: Router, private http: HttpClient, private realEstateService: RealEstateService, private searchService: SearchService) {
    }
 
   ngOnInit() {
-    this.realEstate = JSON.parse(this.route.snapshot.params["realEstate"]);
+    this.searchService.findRealEstateById(this.route.snapshot.params["realEstateId"])
+    .subscribe(data => {
+      this.realEstate = data;
+      this.realEstate.imageModel.forEach(y => {
+          this.retrieveResonse = y;
+          this.base64Data = this.retrieveResonse.picByte;
+          this.retrievedImage = 'data:image/jpeg;base64,' + this.base64Data;
+          this.images.push(this.retrievedImage);
+        });
+        this.realEstate.imageModel = this.images;
+      });
     this.isAdmin = localStorage.getItem('role').toLowerCase() == "admin";
   }
 
